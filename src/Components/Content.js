cc21708164bf3286f3c376790e89ef1aa6094bce
spikeganush <50973584+spikeganush@users.noreply.github.com>
2021-06-25 11:29:09 +1000

    first update

---
+++ src/Components/Content.js
@@ -0,0 +1,8 @@
+export function Content(props) {
+    return (
+        <div className="container">
+            <h1>Some text just to write something</h1>
+        </div>
+
+    )
+}
---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+export function Footer(props) {
+    return (
+        <footer className="footer">
+            <div className="container-fluid">
+                <div className="card bg-dark">                   
+                    <div className="card-body text-light">
+                        <h5 className="card-title">This is the footer</h5>
+                        <p className="card-text">Just extra text you can add in your footer to have some information useful.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
